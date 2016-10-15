001fb4192380db47bd849cb18ae741fb29831949
Laurens Groß <Laurens Gro>
2016-10-15 18:57:44 +0200

    Removed unused textures - removed unesed textures - added benny's package

---
+++ TrafficLightControl/Assets/Editor/Bezier.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bezier : MonoBehaviour {
+
+    /// <summary>
+    /// Same as above only with directly calculating the curve instead of calling Lerp().
+    /// 
+    /// B(t) = (1 - t)3 P0 + 3 (1 - t)2 t P1 + 3 (1 - t) t2 P2 + t3 P3
+    /// </summary>
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            oneMinusT * oneMinusT * oneMinusT * p0 +
+            3f * oneMinusT * oneMinusT * t * p1 +
+            3f * oneMinusT * t * t * p2 +
+            t * t * t * p3;
+    }
+
+    /// <summary>
+    /// Calculates the first derivative of a bezier curve.
+    /// This can be used to represent velocity of an object
+    /// moving along the curve.
+    /// 
+    /// B'(t) = 3 (1 - t)2 (P1 - P0) + 6 (1 - t) t (P2 - P1) + 3 t2 (P3 - P2).
+    /// </summary>
+    public static Vector3 GetVelocity(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        var derivative = 3f * oneMinusT * oneMinusT * (p1 - p0) + 6f * oneMinusT * t * (p2 - p1) + 3f * t * t * (p3 - p2);
+
+        return derivative;
+    }
+}
---
+++ TrafficLightControl/Assets/Scripts/SplineMesh.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class SplineMesh : MonoBehaviour
+{
+    public BezierSpline Spline;
+    public int Segments = 32;
+
+    private void Start()
+    {
+        Mesh mesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector3> normals = new List<Vector3>();
+
+        Vector3 Start = Spline.GetPoint(0f);
+        Quaternion rotation = Spline.GetRotation(0);
+        Vector3 left = rotation * Vector3.left;
+        Vector3 right = rotation * Vector3.right;
+        Vector3 up = rotation * Vector3.up;
+        vertices.Add(Start + right);
+        vertices.Add(Start + left);
+        normals.Add(up);
+        normals.Add(up);
+        int triIndex = 0;
+        
+        for (int i = 0; i <= Segments; i++)
+        {
+            float t = (float)i / (float)Segments;
+            Vector3 End = Spline.GetPoint(t);
+            rotation = Spline.GetRotation(t);
+
+            left = rotation * Vector3.left;
+            right = rotation * Vector3.right;
+            up = rotation * Vector3.up;
+
+            vertices.Add(End + right);
+            vertices.Add(End + left);
+            normals.Add(up);
+            normals.Add(up);
+
+            triangles.Add(triIndex);
+            triangles.Add(triIndex + 1);
+            triangles.Add(triIndex + 2);
+
+            triangles.Add(triIndex + 2);
+            triangles.Add(triIndex + 1);
+            triangles.Add(triIndex + 3);
+
+            triIndex += 2;
+
+            Start = End;
+        }
+
+        mesh.SetVertices(vertices);
+        mesh.SetNormals(normals);
+        mesh.SetTriangles(triangles, 0);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // run Start() in scene edtor every frame
+    void OnEnable() { EditorApplication.update += Start; }
+    void OnDisable() { EditorApplication.update -= Start; }
+}
