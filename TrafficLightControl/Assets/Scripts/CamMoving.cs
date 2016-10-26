63cbb529b2cec124f136df8126ecea2623c98c59
Laurens Groß <Laurens Gro>
2016-10-26 20:29:48 +0200

    Add key control - added script to move the cam in the scene

---
+++ TrafficLightControl/Assets/Scripts/CamMoving.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMoving : MonoBehaviour {
+
+    public float maxLeft;
+    public float maxRight;
+    public float maxForward;
+    public float maxBackward;
+    public float maxZoomIn = 16.0f;
+    public float maxZoomOut = 88.5f;
+
+    public float zoomSpeed = 0.8f;
+    public float movingSpeed = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            moveForward();
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            moveForward(false);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            moveRight();
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            moveRight(false);
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0) {
+            zoomIn();
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+            zoomIn(false);
+        }
+    }
+
+    /// <summary>
+    /// Translate this object in z/-z direction
+    /// </summary>
+    /// <param name="forward"></param>
+    void moveForward(bool forward = true) {
+
+        float speed = movingSpeed;
+
+        if (!forward)
+            speed *= -1;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+    }
+
+    /// <summary>
+    /// Translate this object in x/-x direction
+    /// </summary>
+    /// <param name="forward"></param>
+    void moveRight(bool right = true) {
+
+        float speed = movingSpeed;
+
+        if (!right)
+            speed *= -1;
+
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z );
+    }
+
+    /// <summary>
+    /// Translate this object in y/-y direction
+    /// </summary>
+    /// <param name="zoomIn"></param>
+    void zoomIn(bool zoomIn = true) {
+        float speed = zoomSpeed;
+
+        if (!zoomIn)
+            speed *= -1;
+
+        Vector3 posTmp = transform.position;
+        Vector3 newPos = posTmp;
+        
+        newPos = new Vector3(posTmp.x, posTmp.y + speed, posTmp.z);
+        
+        //limit zoom
+        if (newPos.y >= (maxZoomIn) && newPos.y <= (maxZoomOut + zoomSpeed))
+            transform.position = newPos;
+    }
+
+    /// <summary>
+    /// set the postion of the camera
+    /// </summary>
+    /// <param name="position"></param>
+    public void setCamPosition(Vector3 position) {
+        transform.position = position;
+    }
+}
--- TrafficLightControl/Assets/Scripts/UI.cs
+++ TrafficLightControl/Assets/Scripts/UI.cs
@@ -4,6 +4,18 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour {
 
+    public Vector3 camPos1 = new Vector3(99.4f, 88.5f, 49.7f);
+    public Vector3 camPos2 = new Vector3(79.4f, 34.4f, 59.6f);
+    public Vector3 camPos3 = new Vector3(125.5f, 34.4f, 43.7f);
+    public Vector3 camPos4 = new Vector3(29.0f, 26.4f, 41.8f);
+
+    public Button buttonCamPos1;
+    public Button buttonCamPos2;
+    public Button buttonCamPos3;
+    public Button buttonCamPos4;
+
+    public GameObject Cam;
+
     public Toggle toggle;
 
     public GameObject[] trafficLights;    
@@ -15,7 +27,12 @@ public class UI : MonoBehaviour {
     }
 
     void Awake() {
-        toggle.onValueChanged.AddListener(boomGateEvent);        
+        toggle.onValueChanged.AddListener(boomGateEvent);
+
+        buttonCamPos1.onClick.AddListener(camButtonEvent1);
+        buttonCamPos2.onClick.AddListener(camButtonEvent2);
+        buttonCamPos3.onClick.AddListener(camButtonEvent3);
+        buttonCamPos4.onClick.AddListener(camButtonEvent4);
     }
 	
 	// Update is called once per frame
@@ -24,11 +41,15 @@ public class UI : MonoBehaviour {
 	}
 
     //####################################################################################################
-
+    /// <summary>
+    ///  set the text of the inputfield array
+    /// </summary>
     private void setTextOfInputField() {
 
-        for(int i = 0; i<trafficLights.Length; i++) {
-            InputFields[i].text = trafficLights[i].GetComponent<TrafficLight>().state.ToString();
+        for (int i = 0; i < trafficLights.Length; i++) {
+            // set text, if array size > i
+            if (i < InputFields.Length)
+                InputFields[i].text = trafficLights[i].GetComponent<TrafficLight>().state.ToString();
         }
     }
 
@@ -45,10 +66,32 @@ public class UI : MonoBehaviour {
         }        
     }
 
-    //####################################################################################################
+
+    /// <summary>
+    /// Call function CamMoving script to set postion of cam
+    /// </summary>
+    /// <param name="postion"></param>
+    void setCamPostion(Vector3 postion) {
+        Cam.GetComponent<CamMoving>().setCamPosition(postion);
+    }
+
+    //########################################## Events  ##########################################################
     void boomGateEvent(bool value) {
         foreach(var boomGate in GameObject.FindGameObjectsWithTag("BoomGate")) {
             boomGate.GetComponent<BoomGate>().isOpen = value;
         }
     }
+
+    void camButtonEvent1() {
+        setCamPostion(camPos1);
+    }
+    void camButtonEvent2() {
+        setCamPostion(camPos2);
+    }
+    void camButtonEvent3() {
+        setCamPostion(camPos3);
+    }
+    void camButtonEvent4() {
+        setCamPostion(camPos4);
+    }   
 }
