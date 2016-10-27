0f85f2cc5bc3563ceee857f07ff91825fc4adb57
Benjamin Montazer <benjamin.montazer@fh-stralsund.de>
2016-10-27 19:20:22 +0200

    Prolog Wrapper as Background Prozess
    - implemented script to execute swiprolog.exe in cmdline asynchrously

--- TrafficLightControl/Assets/Scripts/Job.cs
+++ TrafficLightControl/Assets/Scripts/Job.cs
@@ -5,83 +5,115 @@ using System.IO;
 using System;
 using System.Text;
 
-public class Job : ThreadedJob {
+public class Job : MonoBehaviour
+{
 
     private Process prolog;
     private StreamWriter sw;
+    public const string DELIMITER = "-->";
 
-    //overrides
-    protected override void ThreadFunction() {
-        initPrologProcess();
-    }
+    
 
-    protected override void OnFinished() {
-
-    }
-
-    //"normal" functions
-
-    public void initPrologProcess() {
+    /// <summary>
+    /// Init the swi-prolog.exe in a console window
+    /// </summary>
+    public void initPrologProcess()
+    {
         prolog = new Process();
         prolog.StartInfo.UseShellExecute = false;
         prolog.StartInfo.RedirectStandardOutput = true;
         prolog.StartInfo.RedirectStandardInput = true;
-
+        prolog.StartInfo.CreateNoWindow = true;
+        prolog.StartInfo.Arguments = "-q";
         prolog.StartInfo.FileName = @"C:\Program Files\swipl\bin\swipl.exe";
 
+        // start the process
         prolog.Start();
 
-        //IO
+        //IO: register STDIN of the process as a streamwriter
         sw = new StreamWriter(prolog.StandardInput.BaseStream, Encoding.ASCII);
         sw.AutoFlush = true;
 
-        //Encoding
+        //make sure Encodings are compatible
         Console.OutputEncoding = Encoding.ASCII;
         Console.InputEncoding = Encoding.ASCII;
 
+        // register the recieved listener for STDOUT of the process
         prolog.OutputDataReceived += (sender, args) => {
             printToUnity(args.Data);
         };
 
+        // start listening on STDOUT
         prolog.BeginOutputReadLine();
-        prolog.WaitForExit();
     }
 
-    public void consultFile(string path) {
+
+
+    public void ConsultFile(string path)
+    {
         if (prolog == null)
             return;
 
-        //print("Try to consult: " + path);
-        Console.WriteLine("Try to consult: " + path);
+        print("Try to consult: " + path);
+        //Console.WriteLine("Try to consult: " + path);
         printToUnity("Try to consult: " + path);
 
 
         sw.WriteLine("consult('" + path + "').");
         sw.Flush();
 
-        //string output = prolog.StandardOutput.ReadToEnd();
+        //string output = prolog.StandardOutput.ReadToEnd(); 
         //printToUnity("consult '" + Path.GetFileName(path) + "': " + output);
-
-
     }
 
-    public void printToUnity(string message) {
-        //print("<-- " + message);
-        Console.WriteLine("<-- " + message);
+    /// <summary>
+    /// Print a string to Unity's console and logfile.
+    /// </summary>
+    /// <param name="message"></param>
+    public void printToUnity(string message)
+    {
+        // only print sensible messages
+        if(string.IsNullOrEmpty(message.Trim()))
+            return;
 
-        using (StreamWriter sw2 = new StreamWriter(@".\Assets\Prolog\out.txt")) {
+        // print to console and use delimiter
+        print(DELIMITER + message);
+
+        // print to logfile
+        using (StreamWriter sw2 = new StreamWriter(@".\Assets\Prolog\out.txt"))
+        {
             sw2.WriteLine(message);
             sw2.Flush();
         }
-        
+
     }
 
 
-    public void toProlog(string message) {
-
-        if (sw == null)
+    /// <summary>
+    /// Query swi-prolog for something.
+    /// </summary>
+    /// <param name="message">The query string.</param>
+    public void Query(string message)
+    {
+        // std-in of prolog still registered?
+        // OR: message not null or empty?
+        if (sw == null || string.IsNullOrEmpty(message.Trim()))
             return;
 
-        sw.WriteLine(message);        
-    }   
+        // make sure the query string ends with a '.'
+        if (!message.Trim().EndsWith("."))
+            message = message + ".";
+
+        // write the query to prolog console and execute
+        sw.WriteLine(message);
+    }
+
+
+    /// <summary>
+    /// Expose the kill signal to other classes.
+    /// </summary>
+    public void Kill()
+    {
+        prolog.Kill();
+    }
 }
--- TrafficLightControl/Assets/Scripts/PrologWrapper.cs
+++ TrafficLightControl/Assets/Scripts/PrologWrapper.cs
@@ -1,29 +1,74 @@
-﻿using UnityEngine;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
-using System;
+﻿
 
-public class PrologWrapper : MonoBehaviour {
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using UnityEngine;
+
+public class PrologWrapper : MonoBehaviour
+{
 
     public string[] PrologFiles;
+    private Job _prolog;
 
-    Job prolog;
-
-    // Use this for initialization
-    void Start () {
-
-        prolog = new Job();
-        prolog.initPrologProcess();
+    //Use this for initialization
+    void Start()
+    {
+        //for (int i = 0; i < PrologFiles.Length; i++)
+        //    _prolog.consultFile(PrologFiles[i]);
         
-        for (int i = 0; i < PrologFiles.Length; i++)
-            prolog.consultFile(PrologFiles[i]);
+        // create new backround worker
+        var worker = new BackgroundWorker();
+        // register listeners
+        worker.DoWork += Query;
+        worker.RunWorkerCompleted += HandleResults;
+        // execute worker asynchronously
+        worker.RunWorkerAsync();
+    }
 
-        //prolog.toProlog("gewichtsumme([], 0).");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-       
-	}
+    /// <summary>
+    /// Executed on Main-Thread when worker is done.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void HandleResults(object sender, RunWorkerCompletedEventArgs args)
+    {
+        // swi-prolog is running
+        print("SWI-Prolog is running...");
+    }
+
+    /// <summary>
+    /// Executed asynchronously during run of worker.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void Query(object sender, DoWorkEventArgs args)
+    {
+        print("Launching SWI-Prolog...");
+
+        // start create job object
+        _prolog = new Job();
+
+        // run swi-prolog inside cmd
+        _prolog.initPrologProcess();
+
+        // query for something
+        _prolog.Query("X is 2+6.");
+    }
+
+
+
+    //Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Kill swi-prolog.exe when unity quits.
+    void OnApplicationQuit()
+    {
+        _prolog.Kill();
+    }
 }
