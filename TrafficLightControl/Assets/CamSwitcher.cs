1e49a255309ea96fe58de61466c4c559682b5e0e
Benjamin Montazer <benjamin.montazer@fh-stralsund.de>
2016-11-16 00:36:58 +0100

    Camera Switcher
    - active camera can now be swtiched between top-down and 3rd person with
    'f' or 360's '6' (select) key
    - reduced depth of field effects
    
    
    Former-commit-id: 1d053a6e52d1a1c3d7d5a43b0d7e94284a7d4d7e

---
+++ TrafficLightControl/Assets/CamSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CamSwitcher : MonoBehaviour
+{
+
+    public GameObject[] Cameras;
+    private static int index;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cameras == null)
+            return;
+
+        if (Input.GetButtonUp("SwitchCam"))
+            NextCamera();
+    }
+
+    private void NextCamera()
+    {
+        print(index);
+        // disable currently active camera
+        Cameras[index].SetActive(false);
+
+        // increase index to next camera or back to 0
+        if (++index >= Cameras.Length)
+            index = 0;
+
+        print(index);
+        // enable next camera
+        Cameras[index].SetActive(true);
+    }
+}
