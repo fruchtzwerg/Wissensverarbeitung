d63d90d44e592e0b466de08ac652182a458287bb
zake1987 <User@DESKTOP-0ARSCI1.fritz.box>
2016-11-20 00:25:54 +0100

    Bug behoben
    Former-commit-id: 13dffb63dfbc6ce91cbf9c65bb3000d86ca764e7

--- TrafficLightControl/Assets/Prolog/AStar.pl
+++ TrafficLightControl/Assets/Prolog/AStar.pl
@@ -5,9 +5,53 @@
 :-dynamic s/3.
 :-dynamic goal/1.
 
+:-set_prolog_flag(answer_write_options,
+                   [ quoted(true),
+                     portray(true),
+                     spacing(next_argument)
+                   ]).
+
+
+liste([arc(l1_start,l1_bahnhofstr,5,350),
+       arc(l1_bahnhofstr,l1_sparkasse,5,207),
+       arc(l1_sparkasse,l1_bushalte,5,317),
+       arc(l1_bushalte,l1_jungfernstieg,5,534),
+       arc(l1_jungfernstieg,l1_barther_a,5,308),
+       arc(l1_barther_a,l1_barther_b,5,366),
+       arc(l1_barther_b,l1_barther_b,5,0),
+       arc(l1_jungfernstieg,l1_pflegeheim,5,460),
+       arc(l1_pflegeheim,l1_pflegeheim,5,0),
+       arc(l2_start,l2_bahnhofstr_a,5,355),
+       arc(l2_bahnhofstr_a,l2_bahnhofstr_b,5,201),
+       arc(l2_bahnhofstr_b,l2_bahnhofstr_end,5,355),
+       arc(l2_bahnhofstr_end,l2_bahnhofstr_end,5,0),
+       arc(l2_start,l2_bahnhofstr_straight,5,349),
+       arc(l2_bahnhofstr_straight,l2_sparkasse,5,306),
+       arc(l2_sparkasse,l2_bus,5,753),
+       arc(l2_bus,l2_hbf_straight_a,5,276),
+       arc(l2_hbf_straight_a,l2_hbf_straight_b,5,197),
+       arc(l2_hbf_straight_b,l2_bahn,5,261),
+       arc(l2_bahn,l2_end,5,407),
+       arc(l2_end,l2_end,5,0),
+       arc(l2_bus,l2_hbf_left,5,275),
+       arc(l2_hbf_left,l2_hbf_park,5,242),
+       arc(l2_hbf_park,l2_hbf_park,5,0),
+       arc(l2_sparkasse,l1_bushalte,5,225),
+       arc(l3_start,l3_bahn,5,297),
+       arc(l3_bahn,l3_hbf_a,5,362),
+       arc(l3_hbf_a,l3_hbf_b,5,235),
+       arc(l3_hbf_b,l3_hbf_c,5,255),
+       arc(l3_hbf_c,l3_bus,5,428),
+       arc(l3_bus,l3_bus_left,5,284),
+       arc(l3_bus_left,l2_sparkasse,5,45),
+       arc(l3_bus,l3_bahnhofstr,5,374),
+       arc(l3_bahnhofstr,l3_end,5,583),
+       arc(l3_end,l3_end,5,0),
+       arc(l4_start,l4_bahn,5,326),
+       arc(l4_bahn,l4_hbf_a,5,324),arc(l4_hbf_a,l4_hbf_b,5,232),arc(l4_hbf_b,l4_hbf_c,5,262),arc(l4_hbf_c,l4_bahnhofstr_a,5,784),arc(l4_bahnhofstr_a,l4_bahnhofstr_b,5,288),arc(l4_bahnhofstr_b,l4_end,5,298),arc(l4_end,l4_end,5,0),arc(l4_bahnhofstr_a,l2_bahnhofstr_b,5,167),arc(l4_hbf_a,l2_hbf_park,5,189),arc(barther_start,barther_matratzen,5,471),arc(barther_matratzen,l2_hbf_straight_b,5,152),arc(barther_matratzen,l3_hbf_b,5,237),arc(barther_matratzen,l4_hbf_b,5,258),arc(jungfernstieg_start,jungfernstieg_ampel,5,315),arc(jungfernstieg_ampel,l1_barther_a,5,179),arc(jungfernstieg_ampel,l2_hbf_straight_a,5,200),arc(jungfernstieg_ampel,l3_hbf_c,5,285),arc(jungfernstieg_ampel,l4_hbf_c,5,315),arc(jungfernstieg_ampel,l2_hbf_left,5,220),arc(bahnhofstr_start,bahnhofstr_right,5,335),arc(bahnhofstr_right,l4_bahnhofstr_b,5,166),arc(bahnhofstr_start,bahnhofstr_left,5,337),arc(bahnhofstr_left,l2_sparkasse,5,311),arc(bahnhofstr_left,l1_sparkasse,5,270),arc(hbf_start,l3_hbf_c,5,183),arc(hbf_start,l4_hbf_c,5,175)]).
 
 %Definition des Zieles
-goal(t).
+goal(N).
 
 %Das setzen eines neuen Graphen/Baumes von C# aus
 setNewGraph(List):-
@@ -36,7 +80,7 @@ createGraph([arc(N,N1,Cost,HeuristicValue)|Tail]):-
 %Dabei sind Namen der Start und Zielknoten anzugeben.
 %Als Ergebnis erhält C# die Wegbeschreibung von Knoten Start zu Ziel.
 getPath(Start,Goal,Path):-
-                      retract(goal(_))
+                      retractall(goal(_))
                       ,
                       assert(goal(Goal))
                       ,
@@ -67,9 +111,9 @@ expand( P, l(N,F/G), Bound, Tree1, Solved, Sol)  :-
      !,                                    % Node N has successors
      succlist( G, Succ, Ts),               % Make subtrees Ts
      bestf( Ts, F1),                       % f-value of best successor
-     expand( P, t(N,F1/G,Ts), Bound, Tree1, Solved, Sol)
+     (expand( P, t(N,F1/G,Ts), Bound, Tree1, Solved, Sol)
      ;
-     Solved = never                        % N has no successors - dead end
+     Solved = never)                        % N has no successors - dead end
   ) .
                                          
 %Case 3: non-leaf, f-value less than Bound
